6567a9688ab30716596d3d38e0c7698637068206
Kevin Figueroa <kevmiguelarronte@gmail.com>
2022-11-02 13:41:27 -0400

    Adding 0s

---
+++ Data exploration.sql
@@ -0,0 +1,19 @@
+/*
+Exploratory data analysis of COVID deaths and 
+COVID Vaccination databases
+*/
+
+-- Select relevant data
+SELECT location, date, population, total_cases, 
+new_cases, total_deaths
+FROM [COVID Project]..CovidDeaths
+ORDER BY location, date
+
+-- Create death rate
+SELECT *
+FROM [COVID Project]..CovidDeaths
+
+
+SELECT COUNT(date)
+FROM [COVID Project]..CovidDeaths
+
